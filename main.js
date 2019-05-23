a9f1cd6ff6e046a72ae790b5289d4079fe4ba042
MJ Otto <realmjotto@gmail.com>
2019-05-23 16:06:29 +0200

    finished project

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ const setTime = () => {
     const hours = now.getHours();
 
     const secDeg = ((seconds / 60) * 360);
-    const minDeg = (minutes / 60 * 360);
-    const hourDeg = (hours / 60 * 360);
+    const minDeg = ((minutes / 60) * 360);
+    const hourDeg = ((hours / 12) * 360);
 
     secondpoint.style.transform = `rotate(${secDeg}deg)`;
     minutepoint.style.transform = `rotate(${minDeg}deg)`;
